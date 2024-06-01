73e8efc2e1030610b55fe0909f348c5c6b4d9ef5
Tirthya Kamal Dasgupta <dasguptatirthyakamal@gmail.com>
2024-06-01 18:46:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
 import json
+from typing import Dict
 import pytesseract
 import cv2
 from PIL import Image
 import ftfy
 import pan_read
 import io
+from dotenv import load_dotenv
 
 pytesseract.pytesseract.tesseract_cmd = (
     r"C:\Users\com\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
@@ -32,6 +34,19 @@ def process_image(image_path):
     
     return data
 
+
+def get_env_vars(*args: str) -> Dict[str, str]:
+    env_vars = {}
+
+    for arg in args:
+        env_vars[arg] = os.getenv(arg)
+
+        if not env_vars[arg]:
+            raise EnvironmentError(f"'{arg}'")
+
+    return env_vars
+
+
 def read_images_from_folder(folder_path):
     images_data = []
     
@@ -47,11 +62,25 @@ def read_images_from_folder(folder_path):
 folder_path = r"C:\Users\com\Downloads\PAN_card_OCR\images"  # Replace with your folder path
 images_data = read_images_from_folder(folder_path)
 
-# Save the list of dictionaries to a JSON file
-with io.open("info.json", "w", encoding="utf-8") as outfile:
-    data = json.dumps(
-        images_data, indent=4, sort_keys=True, separators=(",", ": "), ensure_ascii=False
-    )
-    outfile.write(data)
+env_vars = get_env_vars("DB_HOST", "DB_USERNAME", "DB_PASSWORD", "DB_NAME")
 
-print("Processed data saved to info.json")
+DB_HOST = env_vars["DB_HOST"]
+DB_USERNAME = env_vars["DB_USERNAME"]
+DB_PASSWORD = env_vars["DB_PASSWORD"]
+DB_NAME = env_vars["DB_NAME"]
+
+connection = pymysql.connect(host=DB_HOST, user=DB_USERNAME, password=DB_PASSWORD, database=DB_NAME,
+        autocommit=True)
+
+for image_data in images_data:
+    with connection.cursor() as cursor:
+        try:
+            statement = "SET SQL_MODE='ALLOW_INVALID_DATES'"
+
+            cursor.execute(statement)
+
+            statement = f"INSERT INTO pan_card_details (`date_of_birth`, `father_name`, `name`, `pan_card_number`) VALUES (f'{image_data['Date of Birth']}')"
+
+            cursor.execute(statement)
+        except pymysql.Error as err:
+            continue
